35338c39b17c0a254d6b0e70ddcac8e9eaae044f
technicalpits <technical@powerit.my>
2023-11-10 11:08:12 +0800

    insert startup.cs

---
+++ billsbuddy/Startup.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions.Common;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Startup
+{
+    public IConfigurationRoot Configuration { get; private set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //// Add services to the dependency injection container
+        //services.AddMvc(); // Example: Adding MVC services
+        //services.AddTransient<IMyService, MyService>(); // Example: Adding a custom service
+        var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables(); // <--- This picks up env variables
+        Configuration = builder.Build();
+    }
+
+    //public Startup(IHostingEnvironment env)
+    //{
+    //    var builder = new ConfigurationBuilder()
+    //            .AddEnvironmentVariables(); // <--- This picks up env variables
+    //    Configuration = builder.Build();
+    //}.
+}
